3d1c240914a7ccd0a6c9afb1c56db11eb606d458
TrekHub <teddyolweny@gmail.com>
2021-10-06 12:51:25 +0300

    clean up the boiler-plate code

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+ 
+ <h1>Welcome Ted</h1>
+    
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+
+
+</style>
